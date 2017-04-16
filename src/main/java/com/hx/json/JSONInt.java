ad1c3598bd3035920029498f1ffb73823f30118a
Jerry.X.He <970655147@qq.com>
2017-04-16 13:31:08 +0800

    seprate it from HXLog so the log before please visit HXLog :  6fea8ee03767a801b53a77b2596aa08f78a8bace

---
+++ src/main/java/com/hx/json/JSONInt.java
@@ -0,0 +1,67 @@
+package com.hx.json;
+
+import com.hx.json.interf.JSON;
+import com.hx.json.interf.JSONType;
+
+/**
+ * JSONInt
+ *
+ * @author Jerry.X.He <970655147@qq.com>
+ * @version 1.0
+ * @date 4/15/2017 11:58 AM
+ */
+class JSONInt implements JSON {
+
+    /**
+     * 当前JSON持有的val
+     */
+    private int val;
+
+    JSONInt(int val) {
+        this.val = val;
+    }
+
+    @Override
+    public JSONType type() {
+        return JSONType.INT;
+    }
+
+    @Override
+    public Object value() {
+        return Integer.valueOf(val);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return true;
+    }
+
+    @Override
+    public boolean isNull() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public String toString(int indentFactor) {
+        return String.valueOf(val);
+    }
+
+    /**
+     * 根据给定的值创建一个JSONInt
+     *
+     * @param val 给定的值
+     * @return com.hx.log.json.JSONStr
+     * @author Jerry.X.He
+     * @date 4/15/2017 5:18 PM
+     * @since 1.0
+     */
+    static JSONInt fromObject(int val) {
+        return new JSONInt(val);
+    }
+
+}
---
+++ src/main/java/com/hx/json/SimpleJSONConfig.java
@@ -0,0 +1,16 @@
+package com.hx.json;
+
+import com.hx.json.interf.JSONConfig;
+
+/**
+ * JSONConfig的简单实现
+ *
+ * @author Jerry.X.He <970655147@qq.com>
+ * @version 1.0
+ * @date 4/15/2017 12:03 PM
+ */
+public class SimpleJSONConfig implements JSONConfig {
+
+
+
+}
